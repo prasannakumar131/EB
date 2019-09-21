c31cb419a1985428be516da163249d94797db4fd
nayakar <tl@gmail.com>
2019-09-21 20:17:12 +0530

    push ebill

--- APEB/src/main/java/com/cts/eb/controller/EBController.java
+++ APEB/src/main/java/com/cts/eb/controller/EBController.java
@@ -1,12 +1,19 @@
 package com.cts.eb.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.cts.eb.dto.EBDTO;
+import com.cts.eb.service.EBService;
+import com.cts.eb.vo.EBVO;
 
 @Controller
 public class EBController {
+	@Autowired
+	private EBService service;
 
 	@RequestMapping(value="/home.htm")
 	public ModelAndView home() {
@@ -14,9 +21,18 @@ public class EBController {
 	}
 
 	@RequestMapping("/add.htm")
-	public ModelAndView add(@RequestParam int a, @RequestParam int b) {
-		int c = a-b;
-		return new ModelAndView("result", "key", c);
+	public ModelAndView add(@ModelAttribute EBVO ebvo) {
+		EBDTO ebdto=null;
+		ebdto=new EBDTO();
+		ebdto.setServiceNo(Long.parseLong(ebvo.getServiceNo()));
+		ebdto.setCustomerName(ebvo.getCustomerName());
+		ebdto.setAddress(ebvo.getAddress());
+		ebdto.setMobile(Long.parseLong(ebvo.getMobile()));
+		ebdto.setEmail(ebvo.getEmail());
+		ebdto.setUnits(Integer.parseInt(ebvo.getUnits()));
+		
+		ebdto=service.registerCustormer(ebdto);		
+		return new ModelAndView("result", "dtokey", ebdto);
 	}
 
 }
